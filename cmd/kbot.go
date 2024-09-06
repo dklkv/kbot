ca2705c08da231e1ad8c6e9daa0c9b6192b6ec6f
Dmytro Kulykov <kulikov.dmitry@gmail.com>
2024-09-06 14:09:17 +0300

    Add kbot command switch cases

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/stianeikeland/go-rpio"
 	telebot "gopkg.in/telebot.v3"
 )
 
@@ -43,13 +44,53 @@ to quickly create a Cobra application.`,
 			return
 		}
 
+		err = rpio.Open()
+		if err != nil {
+			log.Printf("Unable to open gpio: %s", err.Error())
+		}
+
+		defer rpio.Close()
+
+		trafficSignal := make(map[string]map[string]int8)
+
+		trafficSignal["red"] = make(map[string]int8)
+		trafficSignal["amber"] = make(map[string]int8)
+		trafficSignal["green"] = make(map[string]int8)
+
+		trafficSignal["red"]["pin"] = 12
+		//default on/off
+		//trafficSignal["red"]["on"]=0
+		trafficSignal["amber"]["pin"] = 27
+		trafficSignal["green"]["pin"] = 22
+
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			var (
+				err error
+				pin = rpio.Pin(0)
+			)
+
 			log.Print(m.Message().Payload, m.Text())
 			payload := m.Message().Payload
 
 			switch payload {
 			case "hello":
 				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+
+			case "red", "amber", "green":
+				pin = rpio.Pin(trafficSignal[payload]["pin"])
+				if trafficSignal[payload]["on"] == 0 {
+					pin.Output()
+					trafficSignal[payload]["on"] = 1
+				} else {
+					pin.Input()
+					trafficSignal[payload]["on"] = 0
+				}
+
+				err = m.Send(fmt.Sprintf("Switch %s light signal to %d", payload, trafficSignal[payload]["on"]))
+
+			default:
+				err = m.Send("Usage: /s red|amber|green")
+				
 			}
 
 			return err
